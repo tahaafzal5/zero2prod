7445b540803acffa08c79c9eeb90777730f76247
Taha Afzal <tahaafzal5@hotmail.com>
2024-06-16 21:47:58 -0500

    Ch7: Start sending mock emails in the dev environment using MockServer
    Adds tests as well.

--- src/email_client.rs
+++ src/email_client.rs
@@ -1,18 +1,34 @@
 use crate::domain::SubscriberEmail;
 use reqwest::Client;
+use secrecy::{ExposeSecret, Secret};
 
 pub struct EmailClient {
     http_client: Client,
     base_url: String,
     sender_email: SubscriberEmail,
+    authorization_token: Secret<String>,
+}
+
+#[derive(serde::Serialize)]
+struct SendEmailRequest {
+    from: String,
+    to: String,
+    subject: String,
+    text_body: String,
+    html_body: String,
 }
 
 impl EmailClient {
-    pub fn new(base_url: String, sender_email: SubscriberEmail) -> Self {
+    pub fn new(
+        base_url: String,
+        sender_email: SubscriberEmail,
+        authorization_token: Secret<String>,
+    ) -> Self {
         Self {
             http_client: Client::new(),
             base_url,
             sender_email,
+            authorization_token,
         }
     }
 
@@ -22,7 +38,66 @@ impl EmailClient {
         subject: &str,
         html_content: &str,
         text_content: &str,
-    ) -> Result<(), String> {
-        todo!()
+    ) -> Result<(), reqwest::Error> {
+        let url = format!("{}/email", self.base_url);
+
+        let request_body = SendEmailRequest {
+            from: self.sender_email.as_ref().to_owned(),
+            to: recipient.as_ref().to_owned(),
+            subject: subject.to_owned(),
+            text_body: text_content.to_owned(),
+            html_body: html_content.to_owned(),
+        };
+
+        let _ = self
+            .http_client
+            .post(&url)
+            .header(
+                "X-Postmark-Server-Token",
+                self.authorization_token.expose_secret(),
+            )
+            .json(&request_body)
+            .send()
+            .await?;
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::email_client::EmailClient;
+    use fake::faker::lorem::en::{Paragraph, Sentence};
+    use fake::Faker;
+    use fake::{faker::internet::en::SafeEmail, Fake};
+    use secrecy::Secret;
+    use wiremock::{matchers::any, Mock, MockServer, ResponseTemplate};
+
+    use crate::domain::SubscriberEmail;
+
+    #[tokio::test]
+    async fn send_email_fires_a_request_to_base_url() {
+        let mock_server = MockServer::start().await;
+        let sender_email = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let email_client =
+            EmailClient::new(mock_server.uri(), sender_email, Secret::new(Faker.fake()));
+
+        Mock::given(any())
+            .respond_with(ResponseTemplate::new(200))
+            .expect(1)
+            .mount(&mock_server)
+            .await;
+
+        let subscriber_email = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let subject: String = Sentence(1..2).fake();
+        let content: String = Paragraph(1..10).fake();
+
+        let _ = email_client
+            .send_email(subscriber_email, &subject, &content, &content)
+            .await;
+
+        // Assert
+        // On drop, MockServer will check if its expectation have been verified
+        // or not itself, so no need to manually asset anything here.
     }
 }

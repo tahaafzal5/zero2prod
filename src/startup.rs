2173aa7fdb5bf428d74cbda7d716eb22d3daa1e7
Taha Afzal <tahaafzal5@hotmail.com>
2024-06-22 23:41:12 -0500

    Ch7: Refactor tests to re-use the application logic

--- src/startup.rs
+++ src/startup.rs
@@ -1,5 +1,5 @@
 use crate::{
-    configuration::Settings,
+    configuration::{DatabaseSettings, Settings},
     email_client::EmailClient,
     routes::{health_check, health_check_route, subscribe, subscriptions_route},
 };
@@ -10,36 +10,58 @@ use sqlx::PgPool;
 use std::net::TcpListener;
 use tracing_actix_web::TracingLogger;
 
-pub async fn build(configuration: Settings) -> Result<Server, std::io::Error> {
-    // Application
-    let address = format!(
-        "{}:{}",
-        configuration.application.host, configuration.application.port
-    );
+pub struct Application {
+    server: Server,
+    port: u16,
+}
 
-    let listener = TcpListener::bind(address).expect(&format!(
-        "Failed to bind to port {}",
-        configuration.application.port
-    ));
+impl Application {
+    pub async fn build(configuration: Settings) -> Result<Self, std::io::Error> {
+        // Database
+        let connection_pool = get_connection_pool(&configuration.database);
 
-    // Database
-    let connection_pool = PgPoolOptions::new().connect_lazy_with(configuration.database.with_db());
+        // Email Client
+        let sender_email = configuration
+            .email_client
+            .sender_email()
+            .expect("Invalid sender email address.");
 
-    // Email Client
-    let sender_email = configuration
-        .email_client
-        .sender_email()
-        .expect("Invalid sender email address.");
+        let timeout = configuration.email_client.timeout();
+        let email_client = EmailClient::new(
+            configuration.email_client.base_url,
+            sender_email,
+            configuration.email_client.authorization_token,
+            timeout,
+        );
 
-    let timeout = configuration.email_client.timeout();
-    let email_client = EmailClient::new(
-        configuration.email_client.base_url,
-        sender_email,
-        configuration.email_client.authorization_token,
-        timeout,
-    );
+        // Application
+        let address = format!(
+            "{}:{}",
+            configuration.application.host, configuration.application.port
+        );
 
-    run(listener, connection_pool, email_client)
+        let listener = TcpListener::bind(address).expect(&format!(
+            "Failed to bind to port {}",
+            configuration.application.port
+        ));
+
+        let port = listener.local_addr().unwrap().port();
+        let server = run(listener, connection_pool, email_client)?;
+
+        Ok(Self { server, port })
+    }
+
+    pub fn port(&self) -> u16 {
+        self.port
+    }
+
+    pub async fn run_until_stopped(self) -> Result<(), std::io::Error> {
+        self.server.await
+    }
+}
+
+pub fn get_connection_pool(configuration: &DatabaseSettings) -> PgPool {
+    PgPoolOptions::new().connect_lazy_with(configuration.with_db())
 }
 
 pub fn run(

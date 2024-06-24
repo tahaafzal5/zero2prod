316b0a558df8b1f48fc77d747621d589d75679fc
Taha Afzal <tahaafzal5@hotmail.com>
2024-06-23 19:29:42 -0500

    Ch7: Start using `status` field in the application

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -43,7 +43,7 @@ pub async fn insert_subscriber(
     connection_pool: &web::Data<PgPool>,
 ) -> Result<(), sqlx::Error> {
     sqlx::query!(
-        "INSERT INTO subscriptions (id, email, name, subscribed_at) VALUES ($1, $2, $3, $4)",
+        "INSERT INTO subscriptions (id, email, name, subscribed_at, status) VALUES ($1, $2, $3, $4, 'confirmed')",
         Uuid::new_v4(),
         new_subscriber.email.as_ref(),
         new_subscriber.name.as_ref(),

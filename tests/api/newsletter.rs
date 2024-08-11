40863f6f501fe2b8e96eb8dabb81aae2e97042a1
Taha Afzal <tahaafzal5@hotmail.com>
2024-08-10 22:51:55 -0500

    Ch9: Make sure newsletter endpoint returns 400 for bad data

--- tests/api/newsletter.rs
+++ tests/api/newsletter.rs
@@ -72,6 +72,44 @@ async fn newsletters_are_delivered_to_confirmed_subscribers() {
     assert_eq!(response.status().as_u16(), 200);
 }
 
+#[tokio::test]
+async fn newsletters_returns_400_for_invalid_data() {
+    // Arrange
+    let app = spawn_app().await;
+    let test_cases = vec![
+        (
+            serde_json::json!({
+                    "content": {
+                        "text": "Newsletter body as plain text",
+                        "html": "<p>Newsletter body as HTML</p>",
+            }}),
+            "missing title",
+        ),
+        (
+            serde_json::json!({"title": "Newsletter!"}),
+            "missing content",
+        ),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        // Act
+        let response = reqwest::Client::new()
+            .post(&format!("{}{}", &app.address, publish_newsletter_route()))
+            .json(&invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        // Assert
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        )
+    }
+}
+
 /// Use the public API of the application under test to create
 /// and unconfirmed subscriber
 async fn create_unconfirmed_subscriber(app: &TestApp) -> ConfirmationLinks {

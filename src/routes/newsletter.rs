f9fb3706e6bdf5f16f9770235e17986b3aeeb04d
Taha Afzal <tahaafzal5@hotmail.com>
2024-08-11 16:42:40 -0500

    Ch10: Validate credentials against what is stored in the database

--- src/routes/newsletter.rs
+++ src/routes/newsletter.rs
@@ -10,7 +10,7 @@ use reqwest::{
     header::{self, HeaderValue},
     StatusCode,
 };
-use secrecy::Secret;
+use secrecy::{ExposeSecret, Secret};
 use sqlx::PgPool;
 use std::fmt::Debug;
 
@@ -108,13 +108,44 @@ fn basic_authentication(headers: &HeaderMap) -> Result<Credentials, anyhow::Erro
     })
 }
 
+async fn validate_credentials(
+    credentials: &Credentials,
+    connection_pool: &PgPool,
+) -> Result<uuid::Uuid, PublishError> {
+    let user_id: Option<_> = sqlx::query!(
+        "SELECT user_id
+        FROM users
+        WHERE username = $1 AND password = $2",
+        credentials.username,
+        credentials.password.expose_secret()
+    )
+    .fetch_optional(connection_pool)
+    .await
+    .context("Failed to perform query to validate auth credentials.")
+    .map_err(PublishError::UnexpectedError)?;
+
+    user_id
+        .map(|row| row.user_id)
+        .ok_or_else(|| anyhow::anyhow!("Invalid username or password."))
+        .map_err(PublishError::AuthError)
+}
+
+#[tracing::instrument(
+    name = "Publish a newsletter issue",
+    skip(body, connection_pool, email_client, request),
+    fields(username = tracing::field::Empty, user_id = tracing::field::Empty)
+)]
 pub async fn publish_newsletter(
     connection_pool: web::Data<PgPool>,
     body: web::Json<BodyData>,
     email_client: web::Data<EmailClient>,
     request: HttpRequest,
 ) -> Result<HttpResponse, PublishError> {
-    let _credentials = basic_authentication(request.headers()).map_err(PublishError::AuthError)?;
+    let credentials = basic_authentication(request.headers()).map_err(PublishError::AuthError)?;
+    tracing::Span::current().record("username", tracing::field::display(&credentials.username));
+
+    let user_id = validate_credentials(&credentials, &connection_pool).await?;
+    tracing::Span::current().record("user_id", tracing::field::display(&user_id));
 
     let confirmed_subscribers = get_confirmed_subscribers(&connection_pool).await?;
 

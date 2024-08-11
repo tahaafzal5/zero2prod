f8da42e8a6b43c75a3f5b5846721911deb354391
Taha Afzal <tahaafzal5@hotmail.com>
2024-08-11 00:04:19 -0500

    Ch9: Fetch confirmed subscribers list

--- src/routes/newsletter.rs
+++ src/routes/newsletter.rs
@@ -1,4 +1,8 @@
-use actix_web::{web, HttpResponse};
+use crate::routes::error_chain_fmt;
+use actix_web::{web, HttpResponse, ResponseError};
+use reqwest::StatusCode;
+use sqlx::PgPool;
+use std::fmt::Debug;
 
 #[derive(serde::Deserialize)]
 pub struct BodyData {
@@ -12,8 +16,54 @@ pub struct Content {
     text: String,
 }
 
-pub async fn publish_newsletter(body: web::Json<BodyData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
+struct ConfirmedSubscriber {
+    email: String,
+}
+
+#[derive(thiserror::Error)]
+pub enum PublishError {
+    #[error(transparent)]
+    UnexpectedError(#[from] anyhow::Error),
+}
+
+impl Debug for PublishError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
+
+impl ResponseError for PublishError {
+    fn status_code(&self) -> reqwest::StatusCode {
+        match self {
+            PublishError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
+}
+
+pub async fn publish_newsletter(
+    connection_pool: web::Data<PgPool>,
+    body: web::Json<BodyData>,
+) -> Result<HttpResponse, PublishError> {
+    let confirmed_subscribers = get_confirmed_subscribers(&connection_pool).await?;
+
+    Ok(HttpResponse::Ok().finish())
+}
+
+async fn get_confirmed_subscribers(
+    pool: &PgPool,
+) -> Result<Vec<ConfirmedSubscriber>, anyhow::Error> {
+    let rows = sqlx::query_as!(
+        ConfirmedSubscriber,
+        r#"
+        SELECT email
+        FROM subscriptions
+        WHERE status = 'confirmed'
+        "#,
+    )
+    .fetch_all(pool)
+    .await?;
+
+    Ok(rows)
 }
 
 pub fn publish_newsletter_route() -> String {

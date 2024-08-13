2db9a46d578e579ec1f7caab6367b779158c52cc
Taha Afzal <tahaafzal5@hotmail.com>
2024-08-12 20:36:25 -0500

    Ch10: Non existing user is rejected by /newsletter endpoint

--- tests/api/newsletter.rs
+++ tests/api/newsletter.rs
@@ -1,3 +1,4 @@
+use uuid::Uuid;
 use wiremock::{matchers::method, matchers::path, Mock, ResponseTemplate};
 use zero2prod::{email_client::email_route, routes::publish_newsletter_route};
 
@@ -113,6 +114,36 @@ async fn requests_missing_authorization_are_rejected() {
         .send()
         .await
         .expect("Failed to execute publish newsletter request");
+
+    // Assert
+    assert_eq!(401, response.status().as_u16());
+    assert_eq!(
+        r#"Basic realm="publish""#,
+        response.headers()["WWW-Authenticate"]
+    );
+}
+
+#[tokio::test]
+async fn non_existing_user_is_rejected() {
+    // Arrange
+    let app = spawn_app().await;
+    let username = Uuid::new_v4().to_string();
+    let password = Uuid::new_v4().to_string();
+
+    // Act
+    let response = reqwest::Client::new()
+        .post(&format!("{}{}", &app.address, publish_newsletter_route()))
+        .basic_auth(username, Some(password))
+        .json(&serde_json::json!({
+            "title": "Newsletter title",
+            "content": {
+                "text": "Newsletter body as plain text",
+                "html": "<p>Newsletter body as HTML</p>",
+            }
+        }))
+        .send()
+        .await
+        .expect("Failed to execure request");
 
     // Assert
     assert_eq!(401, response.status().as_u16());

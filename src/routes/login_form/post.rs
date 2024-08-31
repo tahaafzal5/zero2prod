d9b759b8b1c2e07c17cad88f869123958b67e414
Taha Afzal <tahaafzal5@hotmail.com>
2024-08-31 00:53:57 -0500

    Ch10: Add assertion for the redirect to the login failure as well

--- src/routes/login_form/post.rs
+++ src/routes/login_form/post.rs
@@ -1,13 +1,14 @@
 use actix_web::{error::InternalError, web, HttpResponse};
-use hmac::{Hmac, Mac};
 use reqwest::header::LOCATION;
-use secrecy::{ExposeSecret, Secret};
+use secrecy::Secret;
 use sqlx::PgPool;
 
 use crate::{
     authentication::{validate_credentials, AuthError, Credentials},
     routes::{error_chain_fmt, home_route},
 };
+
+use super::login_route;
 
 #[derive(Clone)]
 pub struct HmacSecret(pub Secret<String>);
@@ -33,13 +34,12 @@ impl std::fmt::Debug for LoginError {
 }
 
 #[tracing::instrument(
-    skip(form, pool, secret),
+    skip(form, pool),
     fields(username=tracing::field::Empty, user_id=tracing::field::Empty)
 )]
 pub async fn login(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>,
-    secret: web::Data<HmacSecret>,
 ) -> Result<HttpResponse, InternalError<LoginError>> {
     let credentials = Credentials {
         username: form.0.username,
@@ -60,18 +60,8 @@ pub async fn login(
                 AuthError::UnexpectedError(_) => LoginError::UnexpectedError(error.into()),
             };
 
-            let query_string = format!("error={}", urlencoding::Encoded::new(error.to_string()));
-            let hmac_tag = {
-                let mut mac =
-                    Hmac::<sha2::Sha256>::new_from_slice(secret.0.expose_secret().as_bytes())
-                        .unwrap();
-                mac.update(query_string.as_bytes());
-
-                mac.finalize().into_bytes()
-            };
-
             let response = HttpResponse::SeeOther()
-                .insert_header((LOCATION, format!("/login?{query_string}&tag={hmac_tag:x}")))
+                .insert_header((LOCATION, format!("{}", login_route())))
                 .finish();
 
             Err(InternalError::from_response(error, response))
